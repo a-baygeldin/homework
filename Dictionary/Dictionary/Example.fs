b2943f4cd3f8451a5acaccfa335ef1ca939c6e27
Alexander <a.baygeldin@gmail.com>
2014-05-30 13:25:29 +0400

    Dictionary Added

---
+++ Dictionary/Dictionary/Example.fs
@@ -0,0 +1,33 @@
+﻿module Example
+    
+    open MyMap
+    open System
+    open System.Diagnostics
+
+    let mutable myDictionary = MyMap.Map<_, _> [|(2, "b"); (3, "c"); (4, "d")|]
+    myDictionary <- myDictionary.Add 1 "a"
+    myDictionary <- myDictionary.Remove 4
+    printfn "%A" (myDictionary.Item(1))
+    for i in myDictionary do printfn "%A" i
+
+    //Speed test
+
+    let mutable myMap = new MyMap.Map<int, string>([])
+    let mutable systemMap = new Map<int, string>([])
+     
+    let stopWatch = Stopwatch.StartNew()
+    for i in [1 .. 1000] do
+        myMap <- myMap.Add i ("value" + i.ToString())
+    stopWatch.Stop()
+     
+    printfn "%A" stopWatch.Elapsed.TotalMilliseconds
+     
+    let stopWatchSystem = Stopwatch.StartNew()
+    for i in [1 .. 1000] do
+        systemMap <- systemMap.Add i ("value" + i.ToString())
+    stopWatchSystem.Stop()
+     
+    printfn "%A" stopWatchSystem.Elapsed.TotalMilliseconds
+
+    Console.ReadLine() |> ignore
+
---
+++ Dictionary/Dictionary/Map.fs
@@ -0,0 +1,190 @@
+﻿(*
+    A. Baygeldin (c) 2014
+    Dictionary
+*)
+
+module MyMap
+    
+    open System
+    open System.Collections
+    open System.Collections.Generic
+    
+    (*Tree union*)
+
+    type private Tree<'K, 'V when 'K:comparison and 'V:equality> =
+        | Node of key: 'K * value: 'V * left: Tree<'K, 'V> * right: Tree<'K,'V> * height: int
+        | Empty
+
+    let private height t =
+        match t with
+        | Node(_, _, _, _, h) -> h
+        | Empty -> 0
+
+    let rec private maxNode t =
+        match t with
+        | Node(_, _, _, Empty, _) -> t
+        | Node(_, _, _, right, _) -> maxNode right
+
+    let rec private minNode t =
+        match t with
+        | Node(_, _, Empty, _, _) -> t
+        | Node(_, _, left, _, _) -> minNode left
+
+    let rec private existsKey k t =
+        match t with
+        | Node (k', _, left, right, _) ->
+            if k > k' then existsKey k right elif k < k' then existsKey k left else true
+        | Empty -> false
+    
+    let rec private count t =
+        match t with
+        | Node (_, _, left, right, _) -> count left + count right + 1
+        | Empty -> 0
+
+    let private isEmpty t = 
+        match t with
+        | Empty -> true
+        | _ -> false
+
+    let rec private getValueByKey k t =
+        match t with
+        | Node (k', v', left, right, _) ->
+            if k > k' then getValueByKey k right elif k < k' then getValueByKey k left else v'
+        | Empty -> failwith "Key not found!"
+
+
+    let private balance t = //4 types of rotation...
+        match t with
+        | Node(k, v, left, right, _) ->
+            if height right - height left > 1 then
+                match right with
+                | Node(k', v', left', right', _) ->
+                    if height left' >= height right'  then
+                        match left' with
+                        | Node(k'', v'', left'', right'', _) ->
+                            let leftNode = Node(k, v, left, left'', max (height left) (height left'') + 1)
+                            let rightNode = Node(k', v', right'', right', max (height right'') (height right') + 1)
+                            Node (k'', v'', leftNode, rightNode, max (height leftNode) (height rightNode) + 1)
+                        | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+                    else
+                        match right' with
+                        | Node(k'', v'', left'', right'', _) ->
+                            let leftNode = Node(k, v, left, left', max (height left) (height left') + 1)
+                            let rightNode = Node(k'', v'', left'', right'', max (height left'') (height right'') + 1)
+                            Node (k', v', leftNode, rightNode, max (height leftNode) (height rightNode) + 1)
+                        | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+                | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+            elif height left - height right > 1 then
+                match left with
+                | Node(k', v', left', right', _) ->
+                    if height right' >= height left' then
+                        match right' with
+                        | Node(k'', v'', left'', right'', _) ->
+                            let leftNode = Node(k', v', left', left'', max (height left') (height left'') + 1)
+                            let rightNode = Node(k, v, right'', right, max (height right'') (height right) + 1)
+                            Node (k'', v'', leftNode, rightNode, max (height leftNode) (height rightNode) + 1)
+                        | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+                    else
+                        match left' with
+                        | Node(k'', v'', left'', right'', _) ->
+                            let leftNode = Node(k'', v'', left'', right'', max (height left'') (height right'') + 1)
+                            let rightNode = Node(k, v, right', right, max (height right') (height right) + 1)
+                            Node (k', v', leftNode, rightNode, max (height leftNode) (height rightNode) + 1)
+                        | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+                | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+            else t
+        | Empty -> failwith "Can't balance tree. Possibly nodes contain wrong heights."
+                
+
+    let rec private add k v t = //non-tail...
+        match t with
+        | Node(k', _, _, _, _) when k = k' ->  t
+        | Node(k', v', left, right, _) -> 
+            let node = if k < k' then add k v left else add k v right
+            if k < k' then
+                Node(k', v', node, right, max (height node) (height right) + 1) 
+            else
+                Node(k', v', left, node, max (height left) (height node) + 1)
+            |> balance  
+        | Empty -> Node(k, v, Empty, Empty, 1)
+
+    let rec private remove k t = //non-tail...
+        match t with
+        | Node(k', _, Empty, _, _) | Node(k', _, _, Empty, _) when k = k' -> Empty
+        | Node(k', _, left, right, _) when k = k' -> 
+            let leaf = if height left >= height right then maxNode left else minNode right
+            match leaf with
+            | Node(k'', v'', _, _, _) -> 
+                let node = if height left >= height right then remove k'' left else remove k'' right
+                if height left >= height right then
+                    Node(k'', v'', node, right, max (height node) (height right) + 1)
+                else
+                    Node(k'', v'', left, node, max (height left) (height node) + 1)
+        | Node(k', v', left, right, _) ->
+            let node = if k < k' then remove k left else remove k right
+            if k < k' then
+                Node(k', v', node, right, max (height node) (height right) + 1) 
+            else
+                Node(k', v', left, node, max (height left) (height node) + 1)
+            |> balance
+        | Empty -> Empty
+        
+
+    (*Map Class*)
+
+    type Map<'K, 'V when 'K:comparison and 'V:equality> =
+        val private t : Tree<'K, 'V> 
+
+        private new (src:Tree<'K, 'V>) =
+            {
+                t = src
+            }
+
+        new (src:seq<'K * 'V>) =
+            {
+                t = if Seq.isEmpty src then Empty else Seq.fold (fun acc (k, v) -> add k v acc) Empty src
+            }
+
+        member this.Add k v = new Map<_, _> (add k v this.t)
+        member this.ContainsKey k = existsKey k this.t
+        member this.Remove k = new Map<_, _> (remove k this.t)
+        member this.Count = count this.t
+        member this.IsEmpty = isEmpty this.t
+        member this.Item k = getValueByKey k this.t
+        member this.TryFind k = if existsKey k this.t then Some (getValueByKey k this.t) else None
+        
+        member private this.GetEnumerator() = 
+            let rec toList t = 
+                match t with
+                | Node(k, v, left, right, _) -> (k, v) :: (toList left) @ (toList right)
+                | Empty -> [] 
+            let list =  toList this.t
+            let current = ref list
+            let head = ref true
+            let getCurrent() = if !head then failwith("Can't take current") else (!current).Head    
+            {
+                new IEnumerator<'K * 'V> with
+                    member this.Current = getCurrent()     
+                interface IEnumerator with
+                    member this.Current = getCurrent() :> obj
+                    member this.MoveNext() = 
+                        if !head then head := false
+                        else current := (!current).Tail
+                        not (!current).IsEmpty
+                    member this.Reset() = 
+                        head := true
+                        current := list
+                interface IDisposable with
+                    member this.Dispose() = ()
+            }
+
+        
+        interface IEnumerable<'K * 'V> with
+            member this.GetEnumerator() = this.GetEnumerator()
+        interface IEnumerable with
+            member this.GetEnumerator() = this.GetEnumerator() :> IEnumerator
+        
+        override this.Equals x =
+            match x with
+            | :? Map<'K, 'V> as y -> (this.Count = y.Count) && (Seq.forall2 (=) this y)
+            | _ -> false
