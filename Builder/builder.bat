17b6aefa77b77db384cf313f4e6e4acf52461613
Alexander <a.baygeldin@gmail.com>
2014-12-04 01:12:09 +0300

    Builder added

---
+++ Builder/builder.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=%PATH%;"C:\Users\SANDRIK\AppData\Local\GitHub\PortableGit_ed44d00daa128db527396557813e7b68709ed0e2\bin\";"C:\Windows\Microsoft.NET\Framework64\v4.0.30319"
+msbuild Application/Application.sln /p:Configuration=Release;VisualStudioVersion=12.0
